9cba016a7ff14918bce526d43316cffad9e5e3fd
Oscar Caicedo <oscarccd17@gmail.com>
2023-02-07 15:42:50 -0600

    gui with click() function and pop-up window method.

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from tkinter import Tk, Button
 from time import strftime, localtime
+from tkinter.messagebox import showinfo
 
+
+def clicked():
+    time = strftime('Day: %d %b %Y\nTime: %H:%M:%S %p\n', localtime())
+    showinfo(message=time)
+
+root = Tk()
+button = Button(root, text='click it', command = clicked)
+
+button.pack()
+root.mainloop()
