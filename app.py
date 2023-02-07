b7b2b3ab1f68cd6f932e0c7b69ea8725f96211c8
Oscar Caicedo <oscarccd17@gmail.com>
2023-02-07 15:57:40 -0600

    2 gui buttons that print to terminal local and greenwhich time respectively. PP 9.3

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from tkinter import Tk, Button
-from time import strftime, localtime
+from tkinter import Tk, Button, LEFT, RIGHT
+from time import strftime, localtime, gmtime
 from tkinter.messagebox import showinfo
 
 
-def clicked():
+def local():
     time = strftime('Day: %d %b %Y\nTime: %H:%M:%S %p\n', localtime())
-    showinfo(message=time)
+    # showinfo(message=time)
+    print(time)
+
 
 root = Tk()
-button = Button(root, text='click it', command = clicked)
+button = Button(root, text='Click for Local Time', command=local)
+button.pack(side=LEFT)
 
-button.pack()
-# root.mainloop()
+
+def greenwhich():
+    time = strftime('Day: %d %b %Y\nTime: %H:%M:%S %p\n', gmtime())
+    # showinfo(message=time)
+    print(time)
+
+
+button2 = Button(root, text='Click for Greenwhich Time', command=greenwhich)
+button2.pack(side=RIGHT)
+root.mainloop()
