87d1d1bfbc54704109560453906002b16da416e5
Oscar Caicedo <oscarccd17@gmail.com>
2023-02-08 11:51:18 -0600

    gui to show weekday in pop using Entry() and showinfo() methods.

--- app.py
+++ app.py
@@ -1,25 +1,50 @@
-from tkinter import Tk, Button, LEFT, RIGHT
-from time import strftime, localtime, gmtime
+from tkinter import Tk, Button, LEFT, RIGHT, Label, Entry, END
+from time import strftime, localtime, gmtime, strptime
 from tkinter.messagebox import showinfo
 
 
-def local():
-    time = strftime('Day: %d %b %Y\nTime: %H:%M:%S %p\n', localtime())
-    # showinfo(message=time)
-    print(time)
+# def local():
+#     time = strftime('Day: %d %b %Y\nTime: %H:%M:%S %p\n', localtime())
+#     # showinfo(message=time)
+#     print(time)
 
+
+# root = Tk()
+# button = Button(root, text='Click for Local Time', command=local)
+# button.pack(side=LEFT)
+
+
+# def greenwhich():
+#     time = strftime('Day: %d %b %Y\nTime: %H:%M:%S %p\n', gmtime())
+#     # showinfo(message=time)
+#     print(time)
+
+
+# button2 = Button(root, text='Click for Greenwhich Time', command=greenwhich)
+# button2.pack(side=RIGHT)
+# root.mainloop()
+
+
+def compute():
+    global dateEnt
+
+    date = dateEnt.get()
+    weekday = strftime('%A', strptime(date, '%b %d, %Y'))
+    showinfo(message='{} was a {}'.format(date, weekday))
+    dateEnt.delete(0, END)
 
 root = Tk()
-button = Button(root, text='Click for Local Time', command=local)
-button.pack(side=LEFT)
 
+# label
+label = Label(root, text='Enter date')
+label.grid(row=0, column=0)
 
-def greenwhich():
-    time = strftime('Day: %d %b %Y\nTime: %H:%M:%S %p\n', gmtime())
-    # showinfo(message=time)
-    print(time)
+# entry
+dateEnt = Entry(root)
+dateEnt.grid(row=0, column=1)
 
+# button
+button = Button(root, text='Enter', command=compute)
+button.grid(row=1, column=0, columnspan=2)
 
-button2 = Button(root, text='Click for Greenwhich Time', command=greenwhich)
-button2.pack(side=RIGHT)
 root.mainloop()
