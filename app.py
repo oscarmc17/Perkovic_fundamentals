95587d57eed81b1ef6bf9c43274f737125ff6fe1
Oscar Caicedo <oscarccd17@gmail.com>
2023-02-08 16:12:59 -0600

    starting practice problem 9.5. Initial code.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from tkinter import Tk, Button, LEFT, RIGHT, Label, Entry, END
+from tkinter import Tk, Button, LEFT, RIGHT, Label, Entry, END, Text, BOTH
 from time import strftime, localtime, gmtime, strptime
 from tkinter.messagebox import showinfo
 
-
-# def local():
+# ------------------------------- 9.3 -------------------------------
 #     time = strftime('Day: %d %b %Y\nTime: %H:%M:%S %p\n', localtime())
 #     # showinfo(message=time)
 #     print(time)
@@ -25,12 +24,16 @@ from tkinter.messagebox import showinfo
 # root.mainloop()
 
 
+# ------------------------------- 9.4 -------------------------------
 # def compute():
 #     global dateEnt
 
 #     date = dateEnt.get()
 #     weekday = strftime('%A', strptime(date, '%b %d, %Y'))
-#     showinfo(message='{} was a {}'.format(date, weekday))
+#     # showinfo(message='{} was a {}'.format(date, weekday))
+#     dateEnt.insert(0, weekday+' ')
+
+# def clear():
 #     dateEnt.delete(0, END)
 
 # root = Tk()
@@ -47,36 +50,20 @@ from tkinter.messagebox import showinfo
 # button = Button(root, text='Enter', command=compute)
 # button.grid(row=1, column=0, columnspan=2)
 
+# # Delete Button
+# delBut = Button(root, command=clear, text="Clear")
+# delBut.grid(row=1, column=0, columnspan=1)
+
 # root.mainloop()
 
-
-def compute():
-    global dateEnt
-
-    date = dateEnt.get()
-    weekday = strftime('%A', strptime(date, '%b %d, %Y'))
-    # showinfo(message='{} was a {}'.format(date, weekday))
-    dateEnt.insert(0, weekday+' ')
-
-def clear():
-    dateEnt.delete(0, END)
+# ------------------------------- 9.5 -------------------------------
+def record(event):
+    print('char = {}'.format(event.keysym))
 
 root = Tk()
-
-# label
-label = Label(root, text='Enter date')
-label.grid(row=0, column=0)
-
-# entry
-dateEnt = Entry(root)
-dateEnt.grid(row=0, column=1)
-
-# button
-button = Button(root, text='Enter', command=compute)
-button.grid(row=1, column=0, columnspan=2)
-
-# Delete Button
-delBut = Button(root, command=clear, text="Clear")
-delBut.grid(row=1, column=0, columnspan=1)
+text = Text(root, width=20, height=5)
+text.bind('<KeyPress>', record)
+text.pack(expand=True, fill=BOTH)
 
 root.mainloop()
+
