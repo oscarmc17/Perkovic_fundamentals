cc10989ff4007d79693c1d36baa54d82072a1131
Oscar Caicedo <oscarccd17@gmail.com>
2023-02-09 15:02:06 -0600

    creating a canvas gui using canvas() method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, LEFT, RIGHT, Label, Entry, END, Text, BOTH
+from tkinter import Tk, Button, LEFT, RIGHT, Label, Entry, END, Text, BOTH, Canvas
 from time import strftime, localtime, gmtime, strptime
 from tkinter.messagebox import showinfo
 
@@ -67,33 +67,64 @@ from tkinter.messagebox import showinfo
 
 # root.mainloop()
 
-def compute():
-    global dateEnt
 
-    date = dateEnt.get()
-    weekday = strftime('%A', strptime(date, '%b %d, %Y'))
-    showinfo(message='{} was a {}'.format(date, weekday))
-    dateEnt.insert(0, weekday+' ')
-    # dateEnt.delete(0, END)
+# def compute():
+#     global dateEnt
 
-def compute2(event):
-    compute()
+#     date = dateEnt.get()
+#     weekday = strftime('%A', strptime(date, '%b %d, %Y'))
+#     showinfo(message='{} was a {}'.format(date, weekday))
+#     dateEnt.insert(0, weekday+' ')
+#     # dateEnt.delete(0, END)
+
+# def compute2(event):
+#     compute()
+
+# root = Tk()
+
+# # label
+# label = Label(root, text='Enter date')
+# label.grid(row=0, column=0)
+
+# # entry
+# dateEnt = Entry(root)
+# dateEnt.grid(row=0, column=1)
+
+# # button
+# button = Button(root, text='Enter', command=compute)
+# button.grid(row=1, column=0, columnspan=2)
+
+# # Bind a key press event handling function record()
+# dateEnt.bind('<Return>', compute2)
+
+# root.mainloop()
+
+
+# ------------------------------- 9.6 -------------------------------
 
 root = Tk()
 
-# label
-label = Label(root, text='Enter date')
-label.grid(row=0, column=0)
+def begin(event):
+    'initializes the start of the curve to mouse position'
+    global oldx, oldy
+    oldx, oldy = event.x, event.y
 
-# entry
-dateEnt = Entry(root)
-dateEnt.grid(row=0, column=1)
 
-# button
-button = Button(root, text='Enter', command=compute)
-button.grid(row=1, column=0, columnspan=2)
+def draw(event):
+    global oldx, oldy, canvas
+    newx, newy = event.x, event.y
 
-# Bind a key press event handling function record()
-dateEnt.bind('<Return>', compute2)
+    canvas.create_line(oldx, oldy, newx, newy)
 
+    oldx, oldy = newx, newy
+
+
+oldx, oldy = 0, 0
+
+canvas = Canvas(root, height=150, width=300)
+
+canvas.bind("<Button-1>", begin)
+canvas.bind("<Button1-Motion>", draw)
+
+canvas.pack()
 root.mainloop()
