124672606e84332305d8a219d9f395a35fcf2744
Oscar Caicedo <oscarccd17@gmail.com>
2023-02-10 15:17:23 -0600

    2 user-defined classes that encapsulate date gui

--- app.py
+++ app.py
@@ -209,7 +209,37 @@ class ClickIt(Frame):
         time = strftime('Day: %d %b %Y\nTime: %H:%M:%S %p\n', localtime())
         showinfo(message=time)
 
+# root = Tk()
+# app = ClickIt(root)
+# app.pack()
+# root.mainloop()
+
+
+class Day(Frame):
+    
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.pack()
+
+        label = Label(self, text='Enter date')
+        label.grid(row=0, column=0)
+
+        self.dateEnt = Entry(self)
+        self.dateEnt.grid(row=0, column=1)
+
+        button = Button(self, text='Enter', command=self.compute)
+        button.grid(row=1, column=0, columnspan=2)
+
+    def compute(self):
+
+        date = self.dateEnt.get()
+        weekday = strftime('%A', strptime(date, '%b %d, %Y'))
+        showinfo(message='{} was a {}'.format(date, weekday))
+        self.dateEnt.delete(0, END)
+
 root = Tk()
-app = ClickIt(root)
-app.pack()
+day = Day(root)
+day.pack()
+clickit = ClickIt(root)
+clickit.pack()
 root.mainloop()
