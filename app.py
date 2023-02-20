25d140745a5398409a064befffb54722c7fedc67
Oscar Caicedo <oscarccd17@gmail.com>
2023-02-19 21:15:28 -0600

    adding .bind(<KeyPress>) of w to move pen up and automate

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, LEFT, RIGHT, Label, Entry, END, Text, BOTH, Canvas, Frame, SUNKEN
+from tkinter import Tk, Button, LEFT, RIGHT, Label, Entry, END, Text, BOTH, Canvas, Frame, SUNKEN, Event
 from time import strftime, localtime, gmtime, strptime
 from tkinter.messagebox import showinfo
 
@@ -278,6 +278,7 @@ class Draw(Frame):
         b2.grid(row=1, column=0, columnspan=1)
         b3.grid(row=1, column=1, columnspan=2)
         b4.grid(row=2, column=0, columnspan=2)
+        b1.bind('<KeyPress-w>', lambda event: self.up())
 
     def up(self):
         self.canvas.create_line(self.x, self.y, self.x, self.y-10)
@@ -298,44 +299,4 @@ class Draw(Frame):
 
 root = Tk()
 app = Draw(root)
-# app.pack()
-app.mainloop()
-# def up():
-#     global y, canvas
-#     canvas.create_line(x, y, x, y-10)
-#     y -= 10
-
-
-# def left():
-#     global x, canvas
-#     canvas.create_line(x, y, x-10, y)
-#     x -= 10
-
-
-# def right():
-#     global x, canvas
-#     canvas.create_line(x, y, x+10, y)
-#     x += 10
-
-
-# def down():
-#     global y, canvas
-#     canvas.create_line(x, y, x, y+10)
-#     y += 10
-
-# canvas = Canvas(root, height=100, width=150, relief=SUNKEN, borderwidth=3)
-# canvas.pack(side=LEFT)
-
-# box = Frame(root)
-# box.pack(side=RIGHT)
-
-# button = Button(box, text='Up', command=up)
-# button.grid(row=0, column=0, columnspan=2)
-# button = Button(box, text='Left', command=left)
-# button.grid(row=1, column=0)
-# button = Button(box, text='right', command=right)
-# button.grid(row=1, column=1)
-# button = Button(box, text='down', command=down)
-# button.grid(row=2, column=0, columnspan=2)
-
-# x, y = 50, 75
+root.mainloop()
