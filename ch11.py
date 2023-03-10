6e037c773c6e42ca94a0f09ecef94319eca6d76d
Oscar Caicedo <oscarccd17@gmail.com>
2023-03-09 21:37:53 -0600

    simple function to that returns google info

--- ch11.py
+++ ch11.py
@@ -1,1 +1,11 @@
 # ------------------------------- 11.1 -------------------------------
+from urllib.request import urlopen
+
+
+def getSource(url):
+    'returns the content of resource specified by url as a string'
+    response = urlopen(url)
+    html = response.read()
+    return html.decode()
+
+print(getSource('http://www.google.com'))
