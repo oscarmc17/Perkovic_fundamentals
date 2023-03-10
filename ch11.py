0593c65d500b21c9c10964a66f007382ba3a7c9c
Oscar Caicedo <oscarccd17@gmail.com>
2023-03-10 15:59:43 -0600

    get news url with .count() for specific keywords

--- ch11.py
+++ ch11.py
@@ -1,11 +1,22 @@
 # ------------------------------- 11.1 -------------------------------
 from urllib.request import urlopen
 
+# def getSource(url):
+#     'returns the content of resource specified by url as a string'
+#     response = urlopen(url)
+#     html = response.read()
+#     return html.decode()
 
-def getSource(url):
-    'returns the content of resource specified by url as a string'
+# print(getSource('http://www.google.com'))
+
+# PRACTICE PROBLEM 11.1
+def news(url, topics):
     response = urlopen(url)
     html = response.read()
-    return html.decode()
+    content = html.decode().lower()
 
-print(getSource('http://www.google.com'))
+    for topic in topics:
+        n = content.count(topic)
+        print('{} appears {} times.'.format(topic, n))
+
+print(news('http://bbc.co.uk', ['economy', 'climate', 'education']))
